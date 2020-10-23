45a3add9f2d0a08c1c945e0b1c99c82587c6fe7a
Abdul <diazabdulm@gmail.com>
2020-10-22 23:45:33 -0400

    added user class, and company class; added google maps api

---
+++ src/User.ts
@@ -0,0 +1,17 @@
+import faker from "faker";
+
+export class User {
+  name: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      latitude: +faker.address.latitude(),
+      longitude: +faker.address.longitude(),
+    };
+  }
+}
