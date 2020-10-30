124b9d68d3ac9212e40d76d8c8e0bbaa415b5ebe
Abdul <diazabdulm@gmail.com>
2020-10-30 18:14:34 -0400

    leveraged the power of interfaces to make Map method addMarker much more scalable, customizable, and flexible

--- src/Map.ts
+++ src/Map.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
 export class Map {
   private googleMap: google.maps.Map;
 
@@ -5,6 +12,16 @@ export class Map {
     this.googleMap = new google.maps.Map(document.getElementById(elementID), {
       zoom: 1,
       center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.latitude,
+        lng: mappable.location.longitude,
+      },
     });
   }
 }
